7a3d25eb9e5dfcbbba6e8af4e3ed2ab2ade5a91f
Ben O'Connor <benoconnor@Bens-MacBook-Pro.local>
2017-04-26 15:22:56 +1000

    peekal signatures

---
+++ signatures/src/Peekal/conf.js
@@ -0,0 +1,29 @@
+/*
+ * NB: for images, if you pass in a relative path ('assets/logo.png'), they will be automatically embedded (base64). If you pass in a URL, it will stay as it is.
+ */
+
+ const defaults = {
+   contactSecondary: 'L3, 155 Queen St, Brisbane QLD 4000',
+   logoUrl: 'https://a.whack.wtf/peekal/logo/logo.png',
+   logoAlt: 'Peekal',
+   website: 'http://www.peekal.com',
+   welcome: 'Hi there,',
+   introParagraph: 'Thanks for writing up this email.<br/> We are delighted to reply with a responsive template.',
+ }
+
+module.exports = [
+  Object.assign({}, {
+    id: 'ben', // Will be appended to the built templates.
+    name: 'Ben O\'Connor',
+    position: 'Founder',
+    contactMain: '<a href="tel:+61435844115"><span>+61 435 844 115</span></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
+    contactMail: 'ben@peekal.com',
+  }, defaults),
+  Object.assign({}, {
+    id: 'will', // Will be appended to the built templates.
+    name: 'Will Hackett',
+    contactMain: '<a href="tel:+61435844115"><span>+61 400 456 777</span></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
+    contactMail: 'will@peekal.com',
+  }, defaults),
+
+]
