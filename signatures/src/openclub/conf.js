5bd17e3e30f373197a7fbcf9efdb3337b753d52a
Will Hackett <will@willhackett.com>
2017-04-26 14:34:08 +1000

    new logos

--- signatures/src/openclub/conf.js
+++ signatures/src/openclub/conf.js
@@ -6,13 +6,13 @@ module.exports = [
   {
     id: 'will', // Will be appended to the built templates.
     name: 'Will Hackett',
-    position: 'Director',
+    position: 'Co-Founder',
     welcome: 'Hi there,',
     introParagraph: 'Thanks for writing up this email.<br/> We are delighted to reply with a responsive template.',
     contactMain: '<a href="tel:+61400456777"><span>+61 400 456 777</span></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
     contactMail: 'will@openclub.co',
     contactSecondary: 'L3, 155 Queen St, Brisbane QLD 4000',
-    logoUrl: 'https://openclubdev.github.io/openclub-assets/images/logo/logo-color.png',
+    logoUrl: 'https://a.whack.wtf/openclub/logo/logo.png',
     logoAlt: 'OpenClub logo',
     website: 'http://www.openclub.co'
   },
@@ -25,20 +25,20 @@ module.exports = [
     contactMain: '<a href="tel:+61435844115"><span>+61 435 844 115</span></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
     contactMail: 'ben@openclub.co',
     contactSecondary: 'L3, 155 Queen St, Brisbane QLD 4000',
-    logoUrl: 'https://openclubdev.github.io/openclub-assets/images/logo/logo-color.png',
+    logoUrl: 'https://a.whack.wtf/openclub/logo/logo.png',
     logoAlt: 'OpenClub logo',
     website: 'http://www.openclub.co'
   },
   {
-    id: 'matt', // Will be appended to the built templates.
-    name: 'Matt Way',
-    position: 'Lead Developer',
+    id: 'tom', // Will be appended to the built templates.
+    name: 'Tom Hackett',
+    position: 'Co-Founder',
     welcome: 'Hi there,',
     introParagraph: 'Thanks for writing up this email.<br/> We are delighted to reply with a responsive template.',
-    contactMain: '<a href="tel:+61421089383"><span>+61 421 089 383</span></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
-    contactMail: 'matt@openclub.co',
+    contactMain: '<a href="tel:+61438926330"><span>+61 438 926 330</span></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
+    contactMail: 'tom@openclub.co',
     contactSecondary: 'L3, 155 Queen St, Brisbane QLD 4000',
-    logoUrl: 'https://openclubdev.github.io/openclub-assets/images/logo/logo-color.png',
+    logoUrl: 'https://a.whack.wtf/openclub/logo/logo.png',
     logoAlt: 'OpenClub logo',
     website: 'http://www.openclub.co'
   }
